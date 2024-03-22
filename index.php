9d9eaa6e4a724838a5199812b9d3dacfbea74d35
raimeiga <yusuke612@hotmail.com>
2024-03-23 08:58:39 +0900

    ファイル作成

---
+++ index.php
@@ -0,0 +1,41 @@
+<!-- Silence is golden. -->
+
+<!DOCTYPE html>
+
+<html lang="en">
+<head>
+<title>tac_navi</title>
+<meta name="description" content="tac_naviの説明" />
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri();?>/style.css" />
+    
+</head>
+<body>
+    <header>
+        
+    </header>
+    <main>
+        <section>
+            
+        </section>
+
+        <section>
+            
+        </section>
+
+        <section>
+            
+        </section>
+        
+    </main>
+    <footer>
+        
+    </footer>
+    
+</body>
+</html>
+
+
+
+

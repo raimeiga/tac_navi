69cd9a80257f6ff0c1eb1d1a8152b59d28b2b9c8
raimeiga <yusuke612@hotmail.com>
2024-03-23 10:08:08 +0900

    いちおう home.phpのコードを全てコピペ。

--- index.php
+++ index.php
@@ -1,19 +1,20 @@
 <!-- Silence is golden. -->
+
 <!DOCTYPE html>
 
 <html lang="en">
 <head>
-<title>tac_navi</title>
-<meta name="description" content="tac_naviの説明" />
+    <title>tac_navi</title>
+    <meta name="description" content="tac_naviの説明" />
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri();?>/style.css" >
+    <link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri();?>/style.css" />
     
 </head>
 <body>
     <header>
-        <img src="<?php echo get_template_directory_uri();?>/images/header_rogo.jpg" alt="ヘッダーロゴ">
-        <p>Tac+NAVI</p>
+        <img id="header_logo" src="<?php echo get_template_directory_uri();?>/images/header_rogo.jpg" alt="ヘッダーロゴ">
+        <p id="company_name">Tac+NAVI</p>
     </header>
     <main>
         <section>

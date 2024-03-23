7c961493d4ab35220ee2b475c094a9302da6b5e1
raimeiga <yusuke612@hotmail.com>
2024-03-23 09:28:46 +0900

    ヘッダーロゴが反映されない状態

--- index.php
+++ index.php
@@ -1,5 +1,4 @@
 <!-- Silence is golden. -->
-
 <!DOCTYPE html>
 
 <html lang="en">
@@ -8,12 +7,13 @@
 <meta name="description" content="tac_naviの説明" />
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri();?>/style.css" />
+    <link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri();?>/style.css" >
     
 </head>
 <body>
     <header>
-        
+        <img src="<?php echo get_template_directory_uri();?>/images/header_rogo.jpg" alt="ヘッダーロゴ">
+        <p>Tac+NAVI</p>
     </header>
     <main>
         <section>

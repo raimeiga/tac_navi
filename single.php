74153a405d83817342586856fa4855cb05d40cf7
raimeiga <yusuke612@hotmail.com>
2024-03-27 22:11:18 +0900

    お知らせ詳細ページ作成・トップのタイトル作成

---
+++ single.php
@@ -0,0 +1,17 @@
+<!-- お知らせ詳細ページ -->
+
+<?php get_header();?>  <!-- ← header.phpからヘッダーのコードを呼び出す -->
+
+
+<main>
+<section id="page-news_detail">
+      <div class="wrapper">
+        <h1 id="title-news_detail_j">お知らせ一覧</h1>
+        <h2 id="title-news_detail_e">news & press</h2>
+      </div>
+</section>
+</main>
+
+
+
+<?php get_footer(); ?>    <!-- ← footer.phpからヘッダーのコードを呼び出す -->

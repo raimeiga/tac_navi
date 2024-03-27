4cdeadac7091b4267515eaf1d0f74348277b0533
raimeiga <yusuke612@hotmail.com>
2024-03-27 22:10:23 +0900

    会社情報ページ作成・トップのタイトル作成

---
+++ page-company.php
@@ -0,0 +1,17 @@
+<!-- 会社情報ページ -->
+
+<?php get_header();?>  <!-- ← header.phpからヘッダーのコードを呼び出す -->
+
+
+<main>
+<section id="page-company">
+      <div class="wrapper">
+        <h1 id="title-company_j">会社情報</h1>
+        <h2 id="title-company_e">company</h2>
+      </div>
+</section>
+</main>
+
+
+
+<?php get_footer(); ?>    <!-- ← footer.phpからヘッダーのコードを呼び出す -->

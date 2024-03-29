427d6fbcdcbe6f5c73deb6bf02cbb99a55344315
raimeiga <yusuke612@hotmail.com>
2024-03-29 12:11:34 +0900

    ロゴをクリック後、トップページに遷移するように修正。リンクのリンク先を修正

--- footer.php
+++ footer.php
@@ -1,11 +1,15 @@
 <footer>
     <div id="footer-box">        
-        <img id="footer_rogo" src="<?php echo get_template_directory_uri();?>/images/footer_rogo.jpg" alt="ヘッダーロゴ">
+        <!-- ロゴをクリックすると、トップページのトップに遷移するようロゴにリンクを設定 -->
+        <a href="<?php echo esc_url(home_url()); ?>#">  
+              <img id="header_rogo" src="<?php echo get_template_directory_uri();?>/images/footer_rogo.jpg" alt="フッターロゴ">
+        </a> 
         <p id="copyright">&copy;2024 IT Consultants, Inc.</p>
-        <nav id="footer_navi">                         
+        <nav id="footer_navi">             
+           <a href="<?php echo esc_url(home_url('/category/news/')); ?>">お知らせ</a>
            <a href="<?php echo esc_url(home_url()); ?>#business" >事業内容</a>
-           <a href="<?php echo esc_url(home_url()); ?>#BUSINESS" >会社情報</a>
-           <a href="<?php echo esc_url(home_url('/category/news/')); ?>" >お問い合わせ</a>
+           <a href="<?php echo esc_url(home_url('/page/company/')); ?>">会社情報</a>
+           <a href="<?php echo esc_url(home_url('/page/contact/')); ?>">お問い合わせ</a>
            <!-- ↑　↓　会社情報のページに遷移するように、URLを書く。会社情報と問い合わせだから、固定ページかな。。。 -->
         </nav>       
       </div>

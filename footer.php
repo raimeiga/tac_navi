57adf4de3e2268eb0e0282b1f1efafe6bebd4426
raimeiga <yusuke612@hotmail.com>
2024-04-01 10:30:00 +0900

    レスポンシブ設定に合わせて、フッターーを調整。ナビ項目の順番を修正

--- footer.php
+++ footer.php
@@ -6,9 +6,9 @@
         </a> 
         <p id="copyright">&copy;2024 IT Consultants, Inc.</p>
         <nav id="footer_navi">             
-           <a href="<?php echo esc_url(home_url('/category/news/')); ?>">お知らせ</a>
            <a href="<?php echo esc_url(home_url()); ?>#business" >事業内容</a>
            <a href="<?php echo esc_url(home_url('/page/company/')); ?>">会社情報</a>
+           <a href="<?php echo esc_url(home_url('/category/news/')); ?>">お知らせ</a>
            <a href="<?php echo esc_url(home_url('/page/contact/')); ?>">お問い合わせ</a>
            <!-- ↑　↓　会社情報のページに遷移するように、URLを書く。会社情報と問い合わせだから、固定ページかな。。。 -->
         </nav>       

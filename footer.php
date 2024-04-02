ac7e52103886d4f1f6e8679125c828eaabb12b3d
raimeiga <yusuke612@hotmail.com>
2024-04-02 23:41:27 +0900

    スペリング修正

--- footer.php
+++ footer.php
@@ -2,7 +2,7 @@
     <div id="footer-box">        
         <!-- ロゴをクリックすると、トップページのトップに遷移するようロゴにリンクを設定 -->
         <a href="<?php echo esc_url(home_url()); ?>#">  
-              <img id="header_rogo" src="<?php echo get_template_directory_uri();?>/images/footer_rogo.jpg" alt="フッターロゴ">
+              <img id="footer_logo" src="<?php echo get_template_directory_uri();?>/images/footer_logo.jpg" alt="フッターロゴ">
         </a> 
         <p id="copyright">&copy;2024 IT Consultants, Inc.</p>
         <nav id="footer_navi">             

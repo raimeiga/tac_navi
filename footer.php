c318ba7f1d6c0b4772a29b12873fe342607b22cd
raimeiga <yusuke612@hotmail.com>
2024-04-03 00:11:27 +0900

    横幅修正

--- footer.php
+++ footer.php
@@ -1,19 +1,21 @@
 <footer>
-    <div id="footer-box">        
+    <div id="footer-box">  
+      <div id="footer-logo-navi">    
         <!-- ロゴをクリックすると、トップページのトップに遷移するようロゴにリンクを設定 -->
         <a href="<?php echo esc_url(home_url()); ?>#">  
               <img id="footer_logo" src="<?php echo get_template_directory_uri();?>/images/footer_logo.jpg" alt="フッターロゴ">
-        </a> 
-        <p id="copyright">&copy;2024 IT Consultants, Inc.</p>
+        </a>         
         <nav id="footer_navi">             
            <a href="<?php echo esc_url(home_url()); ?>#business" >事業内容</a>
            <a href="<?php echo esc_url(home_url('/page/company/')); ?>">会社情報</a>
            <a href="<?php echo esc_url(home_url('/category/news/')); ?>">お知らせ</a>
            <a href="<?php echo esc_url(home_url('/page/contact/')); ?>">お問い合わせ</a>
-           <!-- ↑　↓　会社情報のページに遷移するように、URLを書く。会社情報と問い合わせだから、固定ページかな。。。 -->
+           <!-- ↑↓ 会社情報のページに遷移するように、URLを書く。会社情報と問い合わせだから、固定ページかな。。。 -->
         </nav>       
-      </div>
-    </footer>
-    <?php wp_footer(); ?>
+      </div>  
+      <p id="copyright">&copy;2024 IT Consultants, Inc.</p>
+    </div>
+  </footer>
+  <?php wp_footer(); ?>
 </body>
 </html>

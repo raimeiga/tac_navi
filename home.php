0177071cd59f65a724ee4179bac86fb728f710c5
raimeiga <yusuke612@hotmail.com>
2024-04-04 14:45:18 +0900

    新着情報・お知らせタイトル・本文にリンク設定

--- home.php
+++ home.php
@@ -97,6 +97,56 @@
 
         <section id="news">
           <h3 class="title-news">新着情報・お知らせ</h3>
+          <p class="news_and_info_line"><!-- 投稿を仕切るための直線 --></p>
+          <div id="news-container">
+            <div id="news-item">
+              <?php
+                //取得したい投稿記事などの条件を引数として渡す
+                $args = array(
+                // 投稿タイプ
+                'post_type'      => 'post',
+                // カテゴリー名
+                'category_name' => 'news,press',
+                // 1ページに表示する投稿数
+                'posts_per_page' => 3,
+                );                           
+                // データの取得
+                $posts = get_posts($args);
+              ?>              
+                <!-- ループ処理 -->
+                <?php foreach($posts as $post): ?>
+                <!-- 投稿データをセット -->
+                <?php setup_postdata($post); ?>
+                <!-- カテゴリー名を取得　テラコヤのAI先生より提供 -->
+                <span id="news_and_info_category">
+                  <?php
+                    $categories = get_the_category(); // 現在の投稿に割り当てられたカテゴリーを取得
+                        if ( ! empty( $categories ) ) {
+                            echo esc_html( $categories[0]->name ); // 最初のカテゴリー名を表示
+                        };echo "　　";
+                  ?>
+                </span>      
+                
+                <!-- 投稿の日付を表示 -->
+                <p id="news_and_info_date">
+                  <?php echo get_the_date();
+                    echo "　";  
+                  ?>
+                </p>
+                
+                <a id="news_and_info_link" href="<?php echo get_permalink(); ?>" >
+                  <!-- ↓　投稿のタイトルを表示 -->
+                  <p id="news_and_info_title"><?php the_title();?></p>
+                  <br>
+                  <!-- ↓　本文 -->
+                  <p id="news_and_info_content"><?php echo wp_trim_words( get_the_content(), 55, '…' ); ?></p>         
+                </a>
+                <p class="news_and_info_line"><!-- 投稿を仕切るための直線 --></p>     
+                <?php endforeach; ?>                       
+                <!-- 使用した投稿データをリセット -->
+                <?php wp_reset_postdata(); ?>  
+            </div>                  
+          </div>     
         </section>
         
     </main>

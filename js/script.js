fbdfcdefc0364e83759c127de8b58556f15f3b08
raimeiga <yusuke612@hotmail.com>
2024-04-08 13:14:37 +0900

    イベント処理を追記

---
+++ js/script.js
@@ -0,0 +1,15 @@
+// ここからヘッダー
+const tab_menu_btn = document.getElementById("tab-menu-btn");
+const tab_menu = document.getElementById("tab-menu");
+
+// イベント処理
+tab_menu_btn.addEventListener('click', () => {
+    tab_menu.style.display = 'block';
+  });
+
+
+const tab_menu_close_btn = document.getElementById("tab-menu-close-btn");
+// イベント処理
+tab_menu_close_btn.addEventListener('click', () => {
+    tab_menu.style.display = 'none';
+});

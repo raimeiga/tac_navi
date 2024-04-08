44f4864baf02cb95bf8d91204e154114082783d7
raimeiga <yusuke612@hotmail.com>
2024-04-08 13:57:09 +0900

    タブレット用フッター追記

--- footer.php
+++ footer.php
@@ -15,8 +15,23 @@
       </div>  
       <p id="copyright">&copy;2024 IT Consultants, Inc.</p>
     </div>
+
+    <!-- タブレット用ナビ -->
+    <div id="tab-footer">
+        <a href="<?php echo esc_url(home_url()); ?>#">  
+            <img class="header_logo" src="<?php echo get_template_directory_uri();?>/images/white_logo.jpg" alt="タブレット用メニューのロゴ">
+        </a>
+        <nav id="tab-footer-navi">
+          <a href="<?php echo esc_url(home_url()); ?>#business" >事業内容</a>
+          <a href="<?php echo esc_url(home_url('/page/company/')); ?>" >会社情報</a>
+          <a href="<?php echo esc_url(home_url('/category/news/')); ?>" >お知らせ</a>
+          <a href="<?php echo esc_url(home_url('/page/contact/')); ?>" >お問い合わせ</a>
+        </nav>
+        <p id="copyright">&copy;2024 IT Consultants, Inc.</p>
+      </div>
+
   </footer>
-  <script src="js/script.js"></script>
+  <script type="text/javascript" src="js/script.js"></script>
   <?php wp_footer(); ?>
 </body>
 </html>

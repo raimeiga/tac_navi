a8c7daa960aec41e06c99535c8b968bcca763f90
raimeiga <yusuke612@hotmail.com>
2024-04-13 14:19:13 +0900

    html読み取り後にjavaを読むためのコード（構文？）追記

--- js/script.js
+++ js/script.js
@@ -1,16 +1,32 @@
-// ここからヘッダー
-const tab_menu_btn = document.getElementById("tab-menu-btn");
-const tab_menu = document.getElementById("tab-menu");
+$(function(){
 
-イベント処理
-tab_menu_btn.addEventListener('click', () => {
-    tab_menu.style.display = 'block';
+    /* ここにjQueryのコードを書く */ 
+
+  // ここからヘッダー
+  const tab_menu_btn = document.getElementById("tab-menu-btn");
+  const tab_menu = document.getElementById("tab-menu");
+  console.log(tab_menu_btn);
+  console.log("tab_menu_btn");
+
+  // イベント処理
+  tab_menu_btn.addEventListener('click', () => {
+      tab_menu.style.display = 'block';
+    });
+
+
+  const tab_menu_close_btn = document.getElementById("tab-menu-close-btn");
+  // イベント処理
+  tab_menu_close_btn.addEventListener('click', () => {
+      tab_menu.style.display = 'none';
   });
 
-
-const tab_menu_close_btn = document.getElementById("tab-menu-close-btn");
-// イベント処理
-tab_menu_close_btn.addEventListener('click', () => {
-    tab_menu.style.display = 'none';
 });
 
+// tab_menu_btn.addEventListener('click', function() {
+//   // メニューの表示・非表示を切り替える
+//   if (tab_menu.style.display === 'none') {
+//     tab_menu.style.display = 'block';
+//   } else {
+//     tab_menu.style.display = 'none';
+//   }
+// });
